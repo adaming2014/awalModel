cb39188781a301d37fd58408877e060dca650de1
Antoine <git.alucas@gmail.com>
2014-09-29 12:00:41 +0200

    Initial commit

---
+++ src/fr/adaming/awal/entity/UserImpl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.adaming.awal.entity;
+
+/**
+ *
+ * @author INTI0221
+ */
+public class UserImpl implements User {
+    
+}
