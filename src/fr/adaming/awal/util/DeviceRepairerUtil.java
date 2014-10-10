2042f6f41e7d23903cc9e46e5c09a40bea51742b
Antoine <git.alucas@gmail.com>
2014-10-10 13:57:36 +0200

    Add DeviceRepairer constants

---
+++ src/fr/adaming/awal/util/DeviceRepairerUtil.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.adaming.awal.util;
+
+/**
+ *
+ * @author INTI0221
+ */
+public class DeviceRepairerUtil {
+
+    public static final String STATE_CREATE = "CREATE";
+    public static final String STATE_IN_REPAIR = "IN_REPAIR";
+    public static final String STATE_REPAIR = "REPAIR";
+    public static final String STATE_CLOSED = "CLOSED";
+}
